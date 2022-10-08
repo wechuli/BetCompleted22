5bbe834e1ed85937f59a216b5e859ea957be5e86
Poxito <aritzplazaola3@gmail.com>
2022-10-08 18:00:16 +0200

    Sonar

--- src/test/java/test/EmaitzakIpini/EmaitzakIpiniDABTest.java
+++ src/test/java/test/EmaitzakIpini/EmaitzakIpiniDABTest.java
@@ -20,30 +20,7 @@ public class EmaitzakIpiniDABTest {
 		 
 	@Test
 	public void test() {
-		Team team1= new Team("Almeria");
-		Team team2= new Team("Athletic");
-		Event ev111=new Event(1, "Almeria-Athletic", UtilDate.newDate(2022,10,17), team1, team2);
-		Sport sp1=new Sport("Futbol");
-		sp1.addEvent(ev111);
-		Question q1=ev111.addQuestion("Zeinek irabaziko du partidua?",1);
-		Quote quote111 = q1.addQuote(1.3, "1", q1);
 		
-		try {
-			testDA.open();
-			testDA.createEvent(ev111);
-			testDA.close();
-			
-			try {
-				sut.EmaitzakIpini(quote111);
-			} catch (EventNotFinished e) {
-				fail();
-			}
-			
-		}finally {
-			testDA.open();
-			testDA.removeEvent(ev111);
-			testDA.close();
-		}
 	}
 
 }
